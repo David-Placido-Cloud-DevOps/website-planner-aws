04100eb82d6e8769d26a5742a3de00358021fcd8
Rosmery <weddingplanner@Rosmerys-MacBook-Air.local>
2025-02-16 04:11:53 -0500

    Aprovisionamiento de la Infraestructura

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,7 +2,6 @@ output "s3_bucket_name" {
   value = aws_s3_bucket.data-bucket.bucket
   description = "Nombre del bucket s3"
 }
-
 output "cloudfront_distribution_id" {
   value = aws_cloudfront_distribution.cdn.id
   description = "ID de la distribución de CloudFront"
