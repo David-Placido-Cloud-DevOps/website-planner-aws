ed376b73b426ffe249da57bba00c2b6c3da790a2
David Placido <placidoastupinadavid@gmail.com>
2025-03-22 22:58:04 -0500

    delete files static

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,6 +2,7 @@ output "s3_bucket_name" {
   value = aws_s3_bucket.data-bucket.bucket
   description = "Nombre del bucket s3"
 }
+
 output "cloudfront_distribution_id" {
   value = aws_cloudfront_distribution.cdn.id
   description = "ID de la distribución de CloudFront"
